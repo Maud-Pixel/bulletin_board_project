f524cd9676031e4f1b2cec3c6dd242900f5daa2b
Emi <pieront.emilie@gmail.com>
2020-11-19 12:33:35 +0100

    register

--- register.php
+++ register.php
@@ -4,11 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="register.css">
-    <title>Document</title>
+    <title>Register</title>
 </head>
 <body>
-    <form action="register" class="registerUser">
-        
-    </form>
-</body>
-</html>
+    <!-- <?php include "header.php"; ?> -->
+    <div class="register">
+        <form action="register" class="registerUser" method="POST">
+            <h2>Register</h2>
+            <h3>Username</h3>
+            <input type="text" placeholder="Enter your username" name="username" required id="username">
+            <h3>Email</h3>
+            <input type="text" placeholder="Enter your Email" name="email" required id="email">
+            <h3>Password</h3>
+            <input type="text" placeholder="Enter your Password?" name="password" required id="password">
+            <h3>Confirm Password</h3>
+            <input type="text" placeholder="Re-Enter your Password?" name="confirmPassword" required id="confirmPassword">
+            
+            <input type="submit" id='submit' value='LOGIN' >
+            <script>
+            document.querySelector('#submit').addEventListener('click',()=>{
+            let password = document.querySelector('#password').value;
+            let confirmPassword = document.querySelector('#confirmPassword').value;
+
+            if (password === confirmPassword) {
+
+            } else {
+                alert('Your password does not match!')
+            }
+            })
+        </script>
+        </form>
+    </div>
+    <?php
+        if (isset($_POST["submit"])){
+            $username = $_POST["username"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+        };
+
+    ?> 
+    <!-- <?php include "footer.php"; ?> -->
+    </body>
+    </html>
