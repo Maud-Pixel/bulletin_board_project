97bbd059eb650e4b2de3f62a292a1c188b0af7f7
Emi <pieront.emilie@gmail.com>
2020-11-24 15:42:29 +0100

    modif

--- board.php
+++ board.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Board-Page</title>
 </head>
@@ -17,23 +18,40 @@
                     global $db;
 
                     try {
-                        
-                        $response = $db->query('SELECT * FROM boards');
-                        $datas = $response->fetch();
-                        while ($datas = $response->fetch()){
+                        $responses = $db->query('SELECT * FROM category');
+                        $datass = $responses->fetch();
                     ?>
-                        <h2><?php echo $datas['category']; ?></h2>
-                        <p>
-                        Subject : <?php echo $datas['name']; ?> 
-                        Description : <?php echo $datas['description']; ?>
-                        </p>
+                        <h2><?php echo $datass['category_name']; ?></h2>
+                        
                     <?php
-                        }
+                
                     
                     } catch (PDOException $e) {
                         echo 'Connexion échouée : ' . $e->getMessage();
                     }
-                ?>
+                    try {
+                    $response = $db->query('SELECT * FROM boards');
+                        $datas = $response->fetch();
+                        while ($datas = $response->fetch()){
+                        
+                    ?>
+                    <div class="container" style="background-color:#f9f9f9">
+                        <div class="row row-cols-3" >
+                            <div class="card" style="background-color:green">
+                                <div class="card-body" style="background-color:blue">
+                                    <p class="card-title">Subject : <?php echo $datas['name']; ?> </p>
+                                    <p class="card-text">Description : <?php echo $datas['description']; ?> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } //fin while
+                    }//fin try
+                    catch (PDOException $e) {
+                        echo 'Connexion échouée : ' . $e->getMessage();
+                    }
+                    ?>
+                    
             </div>
         </div>
         <div class="category">
